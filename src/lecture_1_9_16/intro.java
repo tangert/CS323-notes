ccc7f6b216e090133b52cc41cd7bb7bc0031f29a
tangert <tyler@angert.com>
2017-01-03 14:15:49 -0500

    initial commit

---
+++ src/lecture_1_9_16/intro.java
@@ -0,0 +1,12 @@
+package lecture_1_9_16;
+
+/**
+ * Created by tylerangert on 1/1/17.
+ */
+
+public class intro implements {
+
+  public static void main(String[] args) {
+    System.out.println("Hello CS323!");
+  }
+}
